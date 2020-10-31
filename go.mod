c4a5187fea854761ad19e73d005e494d63e4f228
treethought <cpsweene@gmail.com>
2020-10-31 14:27:21 -0700

    use structs for states, add initial generate implementation

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/treethought/markovbot
 
 go 1.14
+
+require (
+	github.com/sirupsen/logrus v1.7.0 // indirect
+	golang.org/x/sys v0.0.0-20201029080932-201ba4db2418 // indirect
+)
--- main.go
+++ main.go
@@ -2,35 +2,143 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"math/rand"
+	"regexp"
 	"strings"
+	"time"
+
+	log "github.com/sirupsen/logrus"
 )
 
-func buildMap(corpus string) (m map[string][]string) {
-	m = make(map[string][]string, 0)
+type Child struct {
+	value  string
+	weight int
+}
 
-	words := strings.Split(corpus, " ")
+type State struct {
+	value    string
+	children map[string]int
+}
+
+type Chain struct {
+	states map[string]State
+}
+
+func cleanString(s string) string {
+	reg, err := regexp.Compile("[^a-zA-Z0-9]+")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return reg.ReplaceAllString(s, " ")
+
+}
+
+func (c *Chain) FromString(s string) {
+	log.Info("Building states from string")
+
+	clean := cleanString(s)
+	words := strings.Split(clean, " ")
+
 	for idx, word := range words {
 		if idx == len(words)-1 {
 			return
 		}
-		next := words[idx+1]
-		_, ok := m[word]
+
+		state, ok := c.states[word]
 		if !ok {
-			m[word] = []string{next}
-			continue
+			state = *new(State)
+			state.value = word
+			state.children = make(map[string]int)
 		}
-		m[word] = append(m[word], next)
+
+		nxt := words[idx+1]
+		_, ok = state.children[nxt]
+		if !ok {
+			state.children[nxt] = 0
+		} else {
+			state.children[nxt]++
+		}
+
+		log.Debug("State: %s, children: %v", state.value, state.children)
+		c.states[word] = state
 
 	}
-	return m
+
+}
+
+func weightedSelection(items map[string]int) string {
+	rand.Seed(time.Now().Unix())
+	flat := []string{}
+	for k, w := range items {
+		n := 0
+		for n <= w {
+			flat = append(flat, k)
+			n++
+		}
+	}
+
+	i := rand.Int() % len(flat)
+	res := flat[i]
+	log.Debugf("Selected %s", res)
+	return res
+
+}
+
+func (c *Chain) Generate() {
+	log.Info("Generating text")
+	length := 20
+	curWord := ""
+	for _, state := range c.states {
+		curWord = state.value
+		break
+	}
+
+	log.Debugf("Initial word: %s", curWord)
+
+	result := []string{curWord}
+
+	i := 0
+	for i < length {
+		cur := c.states[curWord]
+		log.Debugf("Current: %s", cur.value)
+
+		for len(cur.children) == 0 {
+			log.Debugf("No children selecting new word")
+			for _, state := range c.states {
+				cur = state
+				log.Debugf("Current: %s", cur.value)
+			}
+		}
+
+		log.Debugf("Selecting from %v", cur.children)
+		nxt := weightedSelection(cur.children)
+		result = append(result, nxt)
+		curWord = nxt
+		cur = c.states[nxt]
+		log.Debugf("Moved state to %v", cur)
+		log.Debug("")
+		i++
+	}
+
+	log.Info(strings.Join(result, " "))
 
 }
 
 func main() {
+	log.SetLevel(log.InfoLevel)
 
-	m := buildMap("my dog is happy and he likes to run my cat is silly and he likes to eat he is also very cute")
-	for k, v := range m {
-		fmt.Printf("%s: %s\n", k, v)
+	c := new(Chain)
+	c.states = make(map[string]State)
+
+	data, err := ioutil.ReadFile("resources/shakespeare.txt")
+	if err != nil {
+		fmt.Println(err.Error())
+		return
 	}
+
+	c.FromString(string(data))
+	c.Generate()
 
 }
